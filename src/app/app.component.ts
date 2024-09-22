8ea5c9d82259f91479922e2c75a0e333b97db6b5
monakamon <>
2024-09-22 12:41:39 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppReactFlowComponent } from './components/app-react-flow.component';
+import { AppReactSelectComponent } from './components/app-react-select.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppReactFlowComponent, AppReactSelectComponent],
   template: `
     <div>
       <h1>React in Angular Demo</h1>
+      <app-react-flow></app-react-flow>
+      <app-react-select></app-react-select>
     </div>
   `
 })
