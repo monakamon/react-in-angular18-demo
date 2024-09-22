01efcf012f6b3c4197328963c4c407d8683b844a
monakamon <>
2024-09-22 12:12:00 +0900

    init

---
+++ src/app/components/app-react-select/app-react-select.component.tsx
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-react-select',
+  standalone: true,
+  imports: [],
+  template: `
+`,
+  styleUrl: './app-react-select.component.scss'
+})
+export class AppReactSelectComponent {
+
+}
